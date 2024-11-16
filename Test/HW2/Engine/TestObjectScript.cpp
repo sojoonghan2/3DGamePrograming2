c10558383df764f96269235469889cdd7af8e783
민경원 <76027752+sojoonghan2@users.noreply.github.com>
2024-11-16 19:44:29 +0900

    여러 발의 총알과 모든 오브젝트 충돌 처리

--- Test/HW2/Engine/TestObjectScript.cpp
+++ Test/HW2/Engine/TestObjectScript.cpp
@@ -14,55 +14,56 @@ TestObjectScript::~TestObjectScript() {}
 
 void TestObjectScript::LateUpdate()
 {
-	shared_ptr<GameObject> mainObject = GET_SINGLE(SceneManager)->FindObjectByName(L"Main_Object");
+    shared_ptr<GameObject> mainObject = GET_SINGLE(SceneManager)->FindObjectByName(L"Main_Object");
+    Vec3 pos = GetTransform()->GetLocalPosition();
 
-	Vec3 pos = GetTransform()->GetLocalPosition();
+    if (INPUT->GetButton(KEY_TYPE::I))
+        pos += Normalize(GetTransform()->GetLook()) * _speed * DELTA_TIME;
+    if (INPUT->GetButton(KEY_TYPE::K))
+        pos -= Normalize(GetTransform()->GetLook()) * _speed * DELTA_TIME;
+    if (INPUT->GetButton(KEY_TYPE::J))
+        pos -= Normalize(GetTransform()->GetRight()) * _speed * DELTA_TIME;
+    if (INPUT->GetButton(KEY_TYPE::L))
+        pos += Normalize(GetTransform()->GetRight()) * _speed * DELTA_TIME;
 
-	if (INPUT->GetButton(KEY_TYPE::I))
-		pos += Normalize(GetTransform()->GetLook()) * _speed * DELTA_TIME;
+    // 모든 총알에 대해 충돌 검사
+    for (int i = 0; i < 50; ++i)
+    {
+        wstring bulletName = L"Bullet" + to_wstring(i);
+        auto bulletObject = GET_SINGLE(SceneManager)->FindObjectByName(bulletName);
 
-	if (INPUT->GetButton(KEY_TYPE::K))
-		pos -= Normalize(GetTransform()->GetLook()) * _speed * DELTA_TIME;
+        if (bulletObject)  // 총알이 존재하면
+        {
+            auto is_collision = GET_SINGLE(SceneManager)->Collition(GetGameObject(), bulletObject);
+            if (is_collision)
+            {
+                if (!initialized)
+                {
+                    initialized = true;
+                    return;
+                }
 
-	if (INPUT->GetButton(KEY_TYPE::J))
-		pos -= Normalize(GetTransform()->GetRight()) * _speed * DELTA_TIME;
+                // OBJn 형식에서 n 번호 추출
+                wstring objectName = GetGameObject()->GetName();
+                size_t index = objectName.find(L"OBJ");
+                if (index != wstring::npos)
+                {
+                    wstring numberStr = objectName.substr(3);
+                    int objectNumber = std::stoi(numberStr);
+                    wstring particleName = L"PARTICLE" + std::to_wstring(objectNumber);
 
-	if (INPUT->GetButton(KEY_TYPE::L))
-		pos += Normalize(GetTransform()->GetRight()) * _speed * DELTA_TIME;
+                    shared_ptr<GameObject> targetObject = GET_SINGLE(SceneManager)->FindObjectByName(particleName);
+                    if (targetObject && targetObject->GetParticleSystem())
+                    {
+                        targetObject->GetParticleSystem()->ParticleStart();
+                        std::cout << "오브젝트 충돌 발생: ";
+                        std::wcout << particleName << std::endl;
+                    }
+                }
+                break;  // 충돌이 발생하면 더 이상 검사하지 않음
+            }
+        }
+    }
 
-	auto bulletObject = GET_SINGLE(SceneManager)->FindObjectByName(L"Bullet");
-
-	auto is_collision = GET_SINGLE(SceneManager)->Collition(GetGameObject(), bulletObject);
-
-	if (is_collision)
-	{
-		if (!initialized)
-		{
-			initialized = true;
-			return; // 처음에는 충돌 검사를 하지 않음
-		}
-		// OBJn 형식에서 n 번호 추출
-		wstring objectName = GetGameObject()->GetName();
-		size_t index = objectName.find(L"OBJ");
-		if (index != wstring::npos)
-		{
-			// "OBJ" 이후의 번호 추출
-			wstring numberStr = objectName.substr(3);  // "OBJ" 다음의 부분만 남김
-			int objectNumber = std::stoi(numberStr);   // 문자열을 숫자로 변환
-
-			// 해당 번호의 파티클 오브젝트 이름 생성 (PARTICLEn)
-			wstring particleName = L"PARTICLE" + std::to_wstring(objectNumber);
-
-			// 해당 파티클 오브젝트 찾기
-			shared_ptr<GameObject> targetObject = GET_SINGLE(SceneManager)->FindObjectByName(particleName);
-			if (targetObject && targetObject->GetParticleSystem())
-			{
-				// 파티클 시작
-				targetObject->GetParticleSystem()->ParticleStart();
-				std::cout << "오브젝트 충돌 발생: ";
-				std::wcout << particleName << std::endl;
-			}
-		}
-	}
-	GetTransform()->SetLocalPosition(pos);
+    GetTransform()->SetLocalPosition(pos);
 }
